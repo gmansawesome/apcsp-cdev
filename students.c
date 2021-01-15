cda853a82bf6bac79a42a96fbdc3860003bcb81d
Michael wang <wangm1245@my.sduhsd.net>
2021-01-15 01:17:17 +0000

    updated students.c

--- students.c
+++ students.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 
-
-
 // student structure
 //struct Student...
-struct Student {
-        char firstname [50];
-        char lastname [50];
-        int age[5];
-        int studentid[50];
+struct Student
+{
+  char firstName[20];
+  char lastName[20];
+  int age;
+  int id;
 };
+
+void printStudent(struct Student* student)
+{
+  printf("Student: %s %s\n", student->firstName, student->lastName);
+  printf("Age: %d\n", student->age);
+  printf("Id: %d\n", student->id);
+}
+
+
+void printAllStudents(struct Student students[], int num)
+{
+  // call printStudent for each student in students
+  for (int i = 0; i < num; i++) {
+    printf("\n student %d \n", i);
+    printStudent(&students[i]);
+  }
+}
 
 int main()
 {
+  char input[256];
+  int imput[256];
 
   // an array of students
-  //xxx students;
+  struct Student students[10];
 
   int numStudents = 0;
   while (1)
   {
     char c;
-    char input[256];
-
     printf("\nEnter a to add, p to print, q to quit:");
     fgets(input, 256, stdin);
     if (sscanf(input, "%c", &c) != 1) continue;
@@ -39,20 +55,23 @@ int main()
     else if (c == 'a')
     {
       // enter a new student
-      struct Student student;
+      printf("\n First name: ");
+      fgets(input, 256, stdin);
+      sscanf(input, "%s", students[numStudents].firstName);
 
-      printf("\nFirst name:");
-      scanf("%s", &student.firstname);
+      printf("\n Lirst name: ");
+      fgets(input, 256, stdin);
+      sscanf(input, "%s", students[numStudents].lastName);
 
-      printf("\nLast  name:");
-      scanf("%s", &student.lastname);
+      printf("\n Age: ");
+      fgets(input, 256, stdin);
+      sscanf(input, "%d", students[numStudents].age);
 
-      printf("\nAge:");
-      scanf("%d", &student.age);
+      printf("\n Id: ");
+      fgets(input, 256, stdin);
+      sscanf(input, "%d", students[numStudents].id);
 
-      printf("\nID:");
-      scanf("%d", &student.studentid);
-      numStudents++;
+numStudents++;
     }
   }
   
